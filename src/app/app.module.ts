a01b0d070e851d8cabeae9f864cffda9b6273833
krvergara <krvergara@vectoritcgroup.com>
2019-04-17 13:04:48 -0400

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//rutas
+import { APP_ROUTES } from './app.routes';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
@@ -26,7 +29,7 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
