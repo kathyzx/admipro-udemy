56b0d12928ff3e4924973e8e45a3d6f49bb5d298
krvergara <krvergara@vectoritcgroup.com>
2019-04-24 12:22:45 -0400

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,28 @@
-// import {URLSearchParams} from '@angular/common/http';
-// import {RouterModule, Routes} from '@angular/router';
+import {RouterModule,Routes } from '@angular/router';
 
-// import {DashboardComponent} from './pages/dashboard/dashboard.component';
-// import {LoginComponent} from './login/Login.Component';
-// import {ProgressComponent} from './pages/progress/progress.component';
-// import {Graficas1Component} from './pages/graficas1/graficas1.component';
-// import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
-// import { from } from 'rxjs';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {LoginComponent} from './login/login.component';
+ import {ProgressComponent} from './pages/progress/progress.component';
+ import {Graficas1Component} from './pages/graficas1/graficas1.component';
+ import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
+ import {PagesComponent} from './pages/pages.component';
+ import {RegisterComponent} from './login/register.component';
 
 
-// const appRoutes: Routes = [
-//   {path: 'dashboard', component: DashboardComponent},
-//   {path: 'login', component: LoginComponent},
-//   {path: 'register', component: LoginComponent},
-//   {path: 'progress', component: ProgressComponent},
-//   {path: 'graficas1', component: Graficas1Component},
-//   {path: '', redirectTo: 'dashboard', pathMatch: 'Full'},
-//   {path: '**', component: NopagefoundComponent }
-// ];
+const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {path: 'dashboard', component: DashboardComponent },
+      {path: 'progress', component: ProgressComponent},
+      {path: 'graficas1', component: Graficas1Component},
+      {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    ]
+  },
+    {path: 'login', component: LoginComponent },
+    {path: 'register', component: RegisterComponent},
+    {path: '**', component: NopagefoundComponent }
+ ];
 
-// export const APP_ROUTES = RouterModule.forRoot( appRoutes, { useHash: true } );
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true } );
